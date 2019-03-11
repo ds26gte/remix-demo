29aae32bf0f42fa1220226e4cabd4ee17c34faa8
Dorai Sitaram <ds26gte@yahoo.com>
2019-03-11 13:55:05 -0400

    ...

--- bs-flags-pathway/langs/en-us/workbook-page-index.rkt
+++ bs-flags-pathway/langs/en-us/workbook-page-index.rkt
@@ -2,5 +2,8 @@
 ("intro-to-dilations" "intro-to-dilations-wb1.pdf")
 ("intro-to-dilations" "intro-to-dilations-ex1.pdf")
 ("flags-of-the-world" "flags-of-the-world-wbp.pdf")
+("flags-of-the-world" "flags-of-the-world-wb1.pdf")
+("flags-of-the-world" "flags-of-the-world-ex1.pdf")
 ("image-transformations" "image-transformations-wbp.pdf")
+("image-transformations" "image-transformations-wb1.pdf")
 )
